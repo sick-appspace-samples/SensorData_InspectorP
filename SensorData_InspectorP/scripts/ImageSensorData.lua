bfee13071ca6b9411248803ebf07e344320334df
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:39:37 +0200

    'Update'

--- SensorData_InspectorP/scripts/ImageSensorData.lua
+++ SensorData_InspectorP/scripts/ImageSensorData.lua
@@ -1,21 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  SensorData_InspectorP
-
-  Summary:
-  Accessing image metadata for each acquired live image.
-
-  How to Run:
-  A connected InpspectorP6xx device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10).
-  Set a breakpoint on the first row inside the main or processImage function to debug step-by-step.
-  See the acquired images in the image viewer on the DevicePage.
-
-  More Information:
-  See the tutorial "Devices - InspectorP - TriggeringAndAcquisition".
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
